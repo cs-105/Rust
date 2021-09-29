3d82225147cd7fac9fe18a332e12d20cd0177982
tknight20 <tknight@westmont.edu>
2021-09-29 15:36:50 -0700

    added spritesheet and got world coordinate system working

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,31 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::render::{WindowCanvas, Texture};
+use sdl2::rect::{Rect, Point};
 use sdl2::image::{self, LoadTexture, InitFlag};
 use std::time::Duration;
 use std::thread;
 
-const SCREEN_WIDTH: u32 = 800;
-const SCREEN_HEIGHT: u32 = 600;
+const SCREEN_WIDTH: u32 = 2560;
+const SCREEN_HEIGHT: u32 = 1440;
 
-fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
+fn render(
+    canvas: &mut WindowCanvas,
+    color: Color,
+    texture: &Texture,
+    position: Point,
+    sprite: Rect,
+) -> Result<(), String> {
 
     canvas.set_draw_color(color);
     canvas.clear();
 
-    canvas.copy(texture, None, None)?;
+    let (width, height) = canvas.output_size()?;
+
+    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+
+    canvas.copy(texture, sprite, screen_rect)?;
 
     canvas.present();
 
@@ -40,7 +52,10 @@ fn main() -> Result<(), String> {
         .build().unwrap();
 
     let texture_creator = canvas.texture_creator();
-    let texture = texture_creator.load_texture("assets/earth.jpg")?;
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
+
+    let position = Point::new(-100,200);
+    let sprite = Rect::new(0,0,26,36);
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
@@ -63,7 +78,7 @@ fn main() -> Result<(), String> {
 
     i = (i + 1) % 255;
 
-    render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture);
+    render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, position, sprite)?;
 
     thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
 
