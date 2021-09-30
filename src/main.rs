92fa1971ac3b688312e29d1fe6b7a54fe2bb6f8e
tknight20 <tknight@westmont.edu>
2021-09-29 18:05:22 -0700

    added and refined movement system

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,68 @@ use sdl2::image::{self, LoadTexture, InitFlag};
 use std::time::Duration;
 use std::thread;
 
-const SCREEN_WIDTH: u32 = 2560;
-const SCREEN_HEIGHT: u32 = 1440;
+const SCREEN_WIDTH: u32 = 1920;
+const SCREEN_HEIGHT: u32 = 1080;
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum X_Axis {
+
+    Left,
+    Right,
+    Off,
+    Both,
+
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Y_Axis {
+
+    Up,
+    Down,
+    Off,
+    Both,
+
+}
+
+#[derive(Debug)]
+struct Player{
+
+    position: Point,
+    sprite: Rect,
+    speed: i32,
+    direction: (X_Axis, Y_Axis),
+
+}
+
+fn update_player(player: &mut Player){
+
+    use self::X_Axis::*;
+    use self::Y_Axis::*;
+
+    match player.direction.0{
+        Left =>{
+            player.position = player.position.offset(-player.speed, 0);
+        },
+        Right =>{
+            player.position = player.position.offset(player.speed, 0);},
+        X_Axis::Off | X_Axis::Both =>{},
+    };
+    match player.direction.1{
+        Up =>{
+            player.position = player.position.offset(0, -player.speed);},
+        Down =>{
+            player.position = player.position.offset(0, player.speed);},
+        Y_Axis::Off | Y_Axis::Both =>{},
+    };
+
+}
 
 fn render(
     canvas: &mut WindowCanvas,
     color: Color,
     texture: &Texture,
-    position: Point,
-    sprite: Rect,
+    player: &Player,
 ) -> Result<(), String> {
 
     canvas.set_draw_color(color);
@@ -25,10 +78,10 @@ fn render(
 
     let (width, height) = canvas.output_size()?;
 
-    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
-    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+    let screen_position = player.position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, player.sprite.width(), player.sprite.height());
 
-    canvas.copy(texture, sprite, screen_rect)?;
+    canvas.copy(texture, player.sprite, screen_rect)?;
 
     canvas.present();
 
@@ -52,10 +105,16 @@ fn main() -> Result<(), String> {
         .build().unwrap();
 
     let texture_creator = canvas.texture_creator();
-    let texture = texture_creator.load_texture("assets/bardo.png")?;
+    let texture = texture_creator.load_texture("assets/ship.png")?;
 
-    let position = Point::new(-100,200);
-    let sprite = Rect::new(0,0,26,36);
+    let mut player = Player{
+
+        position: Point::new(0,0),
+        sprite: Rect::new(0,0,150,150),
+        speed: 5,
+        direction: (X_Axis::Off, Y_Axis::Off),
+
+    };
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
@@ -69,6 +128,62 @@ fn main() -> Result<(), String> {
                 Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), ..} => {
                     break 'running;
+                }, 
+                Event::KeyDown { keycode: Some(Keycode::W), repeat: false, .. } => {
+                    if(player.direction.1 == Y_Axis::Down){
+                        player.direction.1 = Y_Axis::Both;
+                    } else {
+                        player.direction.1 = Y_Axis::Up;
+                    }
+                },
+                Event::KeyDown { keycode: Some(Keycode::A), repeat: false, .. } => {
+                    if(player.direction.0 == X_Axis::Right){
+                        player.direction.0 = X_Axis::Both;
+                    } else {
+                        player.direction.0 = X_Axis::Left;
+                    }
+                },
+                Event::KeyDown { keycode: Some(Keycode::S), repeat: false, .. } => {
+                    if(player.direction.1 == Y_Axis::Up){
+                        player.direction.1 = Y_Axis::Both;
+                    } else {
+                        player.direction.1 = Y_Axis::Down;
+                    }
+                },
+                Event::KeyDown { keycode: Some(Keycode::D), repeat: false, .. } => {
+                    if(player.direction.0 == X_Axis::Left){
+                        player.direction.0 = X_Axis::Both;
+                    } else {
+                        player.direction.0 = X_Axis::Right;
+                    }
+                },
+                Event::KeyUp { keycode: Some(Keycode::W), repeat: false, .. } => {
+                    if(player.direction.1 == Y_Axis::Both){
+                        player.direction.1 = Y_Axis::Down;
+                    } else {
+                        player.direction.1 = Y_Axis::Off;
+                    }
+                },
+                Event::KeyUp { keycode: Some(Keycode::A), repeat: false, .. } => {
+                    if(player.direction.0 == X_Axis::Both){
+                        player.direction.0 = X_Axis::Right;
+                    } else {
+                        player.direction.0 = X_Axis::Off;
+                    }
+                },
+                Event::KeyUp { keycode: Some(Keycode::S), repeat: false, .. } => {
+                    if(player.direction.1 == Y_Axis::Both){
+                        player.direction.1 = Y_Axis::Up;
+                    } else {
+                        player.direction.1 = Y_Axis::Off;
+                    }
+                },
+                Event::KeyUp { keycode: Some(Keycode::D), repeat: false, .. } => {
+                    if(player.direction.0 == X_Axis::Both){
+                        player.direction.0 = X_Axis::Left;
+                    } else {
+                        player.direction.0 = X_Axis::Off;
+                    }
                 },
                 _ => {}
 
@@ -76,9 +191,11 @@ fn main() -> Result<(), String> {
 
         }
 
+    //Update
     i = (i + 1) % 255;
+    update_player(&mut player);
 
-    render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, position, sprite)?;
+    render(&mut canvas, Color::RGB(0,0,0), &texture, &player)?;
 
     thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
 
