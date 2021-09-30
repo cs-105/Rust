8a686bb740b9d880694e2d98efd72260e822b4ae
tknight20 <tknight@westmont.edu>
2021-09-29 18:36:27 -0700

    added documentation comments

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,9 @@ use std::thread;
 const SCREEN_WIDTH: u32 = 1920;
 const SCREEN_HEIGHT: u32 = 1080;
 
-
+//XAxis Enum tracks the states of the x axis inputs given by the AD keys
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum X_Axis {
+enum XAxis {
 
     Left,
     Right,
@@ -23,8 +23,9 @@ enum X_Axis {
 
 }
 
+//YAxis Enum tracks the states of the y axis inputs given by the WS keys
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Y_Axis {
+enum YAxis {
 
     Up,
     Down,
@@ -33,20 +34,23 @@ enum Y_Axis {
 
 }
 
+//Player Struct keeps track of data about the player avatar
 #[derive(Debug)]
 struct Player{
 
-    position: Point,
-    sprite: Rect,
-    speed: i32,
-    direction: (X_Axis, Y_Axis),
+    position: Point, //2-D Cartesian Point 
+    sprite: Rect, //Dimensions are used to select what to render from the spritesheet
+    speed: i32, //Rate at which position is updated
+    direction: (XAxis, YAxis), //Keeps track of what buttons are being pressed for processing with the update_player function
 
 }
 
+//Function to update the player's position on the screen, processes the direction component of the player passed in
+//TODO: Update function so it moves by player speed on the diagonals too
 fn update_player(player: &mut Player){
 
-    use self::X_Axis::*;
-    use self::Y_Axis::*;
+    use self::XAxis::*;
+    use self::YAxis::*;
 
     match player.direction.0{
         Left =>{
@@ -54,18 +58,19 @@ fn update_player(player: &mut Player){
         },
         Right =>{
             player.position = player.position.offset(player.speed, 0);},
-        X_Axis::Off | X_Axis::Both =>{},
+        XAxis::Off | XAxis::Both =>{},
     };
     match player.direction.1{
         Up =>{
             player.position = player.position.offset(0, -player.speed);},
         Down =>{
             player.position = player.position.offset(0, player.speed);},
-        Y_Axis::Off | Y_Axis::Both =>{},
+        YAxis::Off | YAxis::Both =>{},
     };
 
 }
 
+//update the canvas that is passed in, handles drawing the player sprite into the new position
 fn render(
     canvas: &mut WindowCanvas,
     color: Color,
@@ -104,6 +109,7 @@ fn main() -> Result<(), String> {
         .present_vsync()
         .build().unwrap();
 
+    //create the player ship sprite
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("assets/ship.png")?;
 
@@ -112,7 +118,7 @@ fn main() -> Result<(), String> {
         position: Point::new(0,0),
         sprite: Rect::new(0,0,150,150),
         speed: 5,
-        direction: (X_Axis::Off, Y_Axis::Off),
+        direction: (XAxis::Off, YAxis::Off),
 
     };
 
@@ -122,67 +128,68 @@ fn main() -> Result<(), String> {
     'running: loop{
 
         for event in event_pump.poll_iter(){
-
+            //handling input
             match event{
-
+                //Quit logic
                 Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), ..} => {
                     break 'running;
-                }, 
+                },
+                //Update direction enums when keys are pressed and released
                 Event::KeyDown { keycode: Some(Keycode::W), repeat: false, .. } => {
-                    if(player.direction.1 == Y_Axis::Down){
-                        player.direction.1 = Y_Axis::Both;
+                    if(player.direction.1 == YAxis::Down){
+                        player.direction.1 = YAxis::Both;
                     } else {
-                        player.direction.1 = Y_Axis::Up;
+                        player.direction.1 = YAxis::Up;
                     }
                 },
                 Event::KeyDown { keycode: Some(Keycode::A), repeat: false, .. } => {
-                    if(player.direction.0 == X_Axis::Right){
-                        player.direction.0 = X_Axis::Both;
+                    if(player.direction.0 == XAxis::Right){
+                        player.direction.0 = XAxis::Both;
                     } else {
-                        player.direction.0 = X_Axis::Left;
+                        player.direction.0 = XAxis::Left;
                     }
                 },
                 Event::KeyDown { keycode: Some(Keycode::S), repeat: false, .. } => {
-                    if(player.direction.1 == Y_Axis::Up){
-                        player.direction.1 = Y_Axis::Both;
+                    if(player.direction.1 == YAxis::Up){
+                        player.direction.1 = YAxis::Both;
                     } else {
-                        player.direction.1 = Y_Axis::Down;
+                        player.direction.1 = YAxis::Down;
                     }
                 },
                 Event::KeyDown { keycode: Some(Keycode::D), repeat: false, .. } => {
-                    if(player.direction.0 == X_Axis::Left){
-                        player.direction.0 = X_Axis::Both;
+                    if(player.direction.0 == XAxis::Left){
+                        player.direction.0 = XAxis::Both;
                     } else {
-                        player.direction.0 = X_Axis::Right;
+                        player.direction.0 = XAxis::Right;
                     }
                 },
                 Event::KeyUp { keycode: Some(Keycode::W), repeat: false, .. } => {
-                    if(player.direction.1 == Y_Axis::Both){
-                        player.direction.1 = Y_Axis::Down;
+                    if(player.direction.1 == YAxis::Both){
+                        player.direction.1 = YAxis::Down;
                     } else {
-                        player.direction.1 = Y_Axis::Off;
+                        player.direction.1 = YAxis::Off;
                     }
                 },
                 Event::KeyUp { keycode: Some(Keycode::A), repeat: false, .. } => {
-                    if(player.direction.0 == X_Axis::Both){
-                        player.direction.0 = X_Axis::Right;
+                    if(player.direction.0 == XAxis::Both){
+                        player.direction.0 = XAxis::Right;
                     } else {
-                        player.direction.0 = X_Axis::Off;
+                        player.direction.0 = XAxis::Off;
                     }
                 },
                 Event::KeyUp { keycode: Some(Keycode::S), repeat: false, .. } => {
-                    if(player.direction.1 == Y_Axis::Both){
-                        player.direction.1 = Y_Axis::Up;
+                    if(player.direction.1 == YAxis::Both){
+                        player.direction.1 = YAxis::Up;
                     } else {
-                        player.direction.1 = Y_Axis::Off;
+                        player.direction.1 = YAxis::Off;
                     }
                 },
                 Event::KeyUp { keycode: Some(Keycode::D), repeat: false, .. } => {
-                    if(player.direction.0 == X_Axis::Both){
-                        player.direction.0 = X_Axis::Left;
+                    if(player.direction.0 == XAxis::Both){
+                        player.direction.0 = XAxis::Left;
                     } else {
-                        player.direction.0 = X_Axis::Off;
+                        player.direction.0 = XAxis::Off;
                     }
                 },
                 _ => {}
@@ -197,6 +204,7 @@ fn main() -> Result<(), String> {
 
     render(&mut canvas, Color::RGB(0,0,0), &texture, &player)?;
 
+    //Lmimt to 60 fps
     thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
 
     }
