802ab550207218cd820fc0333b95cd0170906608
CS_Stuff <wmitsuk@westmont.edu>
2021-10-19 16:38:27 -0700

    Include file to read and interact with music file

---
+++ src/music.rs
@@ -0,0 +1,18 @@
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, source::Source};
+
+// Make Stream Handle
+let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+
+// Open Music File
+let file = BufReader::new(File::open("examples/music.ogg").unwrap());
+
+// Decode the Music File
+let source = Decoder::new(file).unwrap();
+
+// Directly Place the sound from Music File.
+stream_handle.play_raw(source.convert_samples());
+
+// Keep main thread going while loop is active for playback.
+std::thread::sleep(std::time::Duration::from_secs(5));
