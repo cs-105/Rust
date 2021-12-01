e777239f12dab853a22a5f465d6634ff0991f0e1
John Panos <jpanos@westmont.edu>
2021-11-30 21:08:56 -0800

    In progress refactor

--- src/music.rs
+++ src/music.rs
@@ -1,47 +1,54 @@
-pub mod music{
+pub mod music {
 
+    use futures::executor::block_on;
+    use rodio::source::{SineWave, Source};
+    use rodio::{Decoder, OutputStream, Sink};
     use std::fs::File;
     use std::io::BufReader;
-    use std::time::Duration;
     use std::thread;
-    use rodio::{Decoder, OutputStream, Sink};
-    use rodio::source::{SineWave, Source};
-    use futures::executor::block_on;
+    use std::time::Duration;
 
-        pub fn main_menu_music(){
-            
+    pub fn main_menu_music() {
+        // Make Stream Handle
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
-            
-            // Make Stream Handle
-            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
-            // Open Music File
-            let file = BufReader::new(File::open("assets/Sample.mp3").unwrap());
 
-            let source = Decoder::new(file).unwrap();
+        // Open Music File
+        let file = BufReader::new(File::open("assets/Sample.mp3").unwrap());
 
-            // Decode the Music File
-            let music_file = stream_handle.play_raw(source.convert_samples());
+        let source = Decoder::new(file).unwrap();
 
-            // Set length or end point for music stream
-            std::thread::sleep(std::time::Duration::from_secs(10));
+        // Decode the Music File
+        let music_file = stream_handle.play_raw(source.convert_samples());
 
-            thread::sleep(Duration::from_millis(500));
+        // Set length or end point for music stream
+        std::thread::sleep(std::time::Duration::from_secs(10));
+
+        thread::sleep(Duration::from_millis(500));
+    }
+
+    pub fn in_game_music() {
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&stream_handle).unwrap();
+
+        // The sound plays in a separate thread. This call will block the current thread until the sink
+        // has finished playing all its queued sounds.
+
+        loop {
+            let received = rx.recv().unwrap();
+
+            if received != None {
+                let file = BufReader::new(File::open("assets/Sample.ogg").unwrap());
+                let source = Decoder::new(file).unwrap();
+                sink.stop();
+                sink.append()
+                sink.play();
+            }
+
+           
         }
 
-        pub fn in_game_music(){
-
-            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-            let sink = Sink::try_new(&stream_handle).unwrap();
-            
-            let file = BufReader::new(File::open("assets/Sample.ogg").unwrap());
-            let source = Decoder::new(file).unwrap();
-            // Add a dummy source of the sake of the example.
-            
-            sink.append(source);
-            
-            // The sound plays in a separate thread. This call will block the current thread until the sink
-            // has finished playing all its queued sounds.
-            sink.sleep_until_end();
-        }
+        // sink.sleep_until_end();
+    }
 }
--- src/player.rs
+++ src/player.rs
@@ -6,18 +6,17 @@ pub mod player {
     use crate::KeyboardInput;
     use core::f32::consts::PI;
     use glam::Vec2;
-    use sdl2::pixels::Color;
     use sdl2::rect::Rect;
     use sdl2::render::Texture;
     use sdl2::render::WindowCanvas;
     use std::ops::Add;
 
-    const SCREEN_WIDTH: u32 = 1920; //Width in pixels
-    const SCREEN_HEIGHT: u32 = 1080; //Height in pixels
+    const SCREEN_WIDTH: u32 = 1280; //Width in pixels
+    const SCREEN_HEIGHT: u32 = 720; //Height in pixels
 
     //dimensions of the player sprite
-    const PLAYER_SPRITE_WIDTH: u32 = 150; //Width in pixels
-    const PLAYER_SPRITE_HEIGHT: u32 = 150; //Height in pixels
+    const PLAYER_SPRITE_WIDTH: u32 = 75; //Width in pixels
+    const PLAYER_SPRITE_HEIGHT: u32 = 58; //Height in pixels
 
     const PLAYER_MOVEMENT_SPEED: f32 = 400.0; //Speed in pixels per second
     const PLAYER_ROTATION_SPEED: f32 = 1.0; //Rotation speed in degrees per second
@@ -39,7 +38,7 @@ pub mod player {
             controller_input: ControllerInput,
         ) {
             // Clone position for our new starting point
-            let mut new_pos: Vec2 = self.pos.clone();
+            let new_pos: Vec2 = self.pos.clone();
             let mut new_angle = self.angle.clone();
             let mut force = Vec2::new(0.0, 0.0);
 
@@ -81,26 +80,26 @@ pub mod player {
 
             // Calculate displacement from forces
             let acceleration = force;
-            let velocity = acceleration * Vec2::new(delta as f32, delta as f32);
-            let mut position = new_pos + (velocity * Vec2::new(delta as f32, delta as f32));
+            let velocity = acceleration * delta as f32;
+            let mut position = new_pos + (velocity * delta as f32);
 
             // TODO: Run this code on every game object that is a physics object
             if position.x > (width + 50.0) {
                 // Right of the screen
                 position.x = -40.0;
-                position.y = SCREEN_HEIGHT as f32 - position.y - 150.0;
+                position.y = SCREEN_HEIGHT as f32 - position.y - PLAYER_SPRITE_WIDTH as f32;
             } else if position.x < -50.0 {
                 // Left of the screen
                 position.x = width + 40.0;
-                position.y = SCREEN_HEIGHT as f32 - position.y - 150.0;
+                position.y = SCREEN_HEIGHT as f32 - position.y - PLAYER_SPRITE_WIDTH as f32;
             } else if position.y > (height + 50.0) {
                 // Bottom of the screen
                 position.y = -40.0;
-                position.x = SCREEN_WIDTH as f32 - position.x - 150.0;
+                position.x = SCREEN_WIDTH as f32 - position.x - PLAYER_SPRITE_WIDTH as f32;
             } else if position.y < -50.0 {
                 // Top of the screen
                 position.y = height + 40.0;
-                position.x = SCREEN_WIDTH as f32 - position.x - 150.0;
+                position.x = SCREEN_WIDTH as f32 - position.x - PLAYER_SPRITE_WIDTH as f32;
             }
 
             self.angle = new_angle;
@@ -109,8 +108,6 @@ pub mod player {
     }
     impl Renderable for Player {
         fn render(&self, canvas: &mut WindowCanvas) {
-            canvas.clear();
-            canvas.set_draw_color(Color::RGB(0, 0, 0));
             let rect = Rect::new(
                 self.pos.x as i32,
                 self.pos.y as i32,
@@ -128,7 +125,6 @@ pub mod player {
                     false,
                 )
                 .ok();
-            canvas.present();
         }
         fn set_sprite() {
             todo!()
